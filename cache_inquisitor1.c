456e94e664c695adb9f2ebc9f292702afc609c4d
jmathews93 <mathews1@bu.edu>
2021-04-14 22:14:46 -0400

    asd

--- cache_inquisitor1.c
+++ cache_inquisitor1.c
@@ -27,18 +27,9 @@ long custom_random(int min, int max) {
     return min + rand() / (RAND_MAX / (max-min+1) + 1);
 }
 
-void access_array_randomly(int* array, long array_size, int loop_count) {
-    for (int i = 0; i < loop_count; i++) {
-        int target_index = custom_random(0, array_size);
 
-        if (target_index % 8 == 0) {
-            dont_forget_me_plz = array[target_index] % 2;
-        }
-    }
-}
-
-void access_array_offset(int* array, long array_size, int loop_count, long offset) {
-    for(int i = 0; i < (array_size/offset); i+= offset){
+void access_array_offset(int* array, long array_size, long offset) {
+    for(int i = 0; i < offset; i++){
         dont_forget_me_plz = array[i];
     }
 }
@@ -66,27 +57,31 @@ int main() {
     int step = sizeof(int);
     int count = 0;
     long pp =  (long)pow(step, 20);
-    long offset = 16;
+    long offset = 1;
     int* byte_array = (int*)malloc(sizeof(int) * array_size);
+    populate_array(byte_array, array_size);
 
     printf("%ld\n", pp);
     // for (int i = step; i < (sizeof(int)<<28); i*=step) {
-    for (long i = step; i < pp; i*=step) {
+    for (long i = 1; i < 32; i++) {
         // array_size = i;
-        offset = i;
-        count++;
+        // offset = i;
+        // count++;
         
-        populate_array(byte_array, array_size);
-        clock_t initial_time = clock();
-        access_array_offset(byte_array, array_size, 10000000, offset);
-        clock_t final_time = clock();
-        double time_diff = 1000 * ((double) final_time - initial_time)/ CLOCKS_PER_SEC;
+        for(int j = 0; j < 50; j++){
+            clock_t initial_time = clock();
+            access_array_offset(byte_array, 10000000, i);
+            clock_t final_time = clock();
+            double time_diff = 1000 * ((double) final_time - initial_time)/ CLOCKS_PER_SEC;
 
-        printf("offset: %lu, ", offset);
-        printf("count: %d,", count);
-        printf("time diff: %f,", time_diff);
-        printf("array size: %lu,", array_size);
-        printf("%lu,\n", dont_forget_me_plz);
+            printf("offset: %lu, ", i);
+            // printf("count: %d,", count);
+            printf("time diff: %f,", time_diff);
+            printf("array size: %lu,", array_size);
+            printf("%lu,\n", dont_forget_me_plz);
+        }
+        printf("-------------------------------------\n");
+        
 
         // sleep(1);
     }
